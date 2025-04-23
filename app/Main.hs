d0b72f1224a664d23adafc4ffa84bb2f7f501c6f
givingdonation <me@fano.club>
2025-04-22 22:50:39 -0700

    interpreter completed

---
+++ app/Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Text.Parsec
+import Text.ParserCombinators.Parsec.Number ( int )
+import Data.Either (fromRight)
+
+data Express a = App (Express a) (Express a) | Lam (Express a) | Express a deriving (Eq, Show)
+
+main :: IO ()
+main = loop 0
+
+loop n = do
+  a <- getLine
+  putStrLn $ deParse $ applyReduction $ fromRight (Express (-69420)) $ parse myParser "" a
+  loop $ n + 1
+
+myParser = try $ Express <$> int <|> try lamParser <|> appParser
+
+lamParser = Lam <$> (char 'l' >> myParser)
+
+appParser = App <$> (char '(' >> myParser <* char ' ') <*> myParser <* char ')'
+
+deParse (Lam exp) = "l" ++ deParse exp
+deParse (App a b) = "(" ++ deParse a ++ " " ++ deParse b ++ ")"
+deParse (Express a) = show a
+
+betaReduce :: Int -> Express Int -> Express Int -> Express Int
+betaReduce depth (Express leafInt) n = if depth == leafInt then n else Express leafInt
+betaReduce depth (Lam a) n = Lam $ betaReduce (depth + 1) a n
+betaReduce depth (App a b) n = App (betaReduce depth a n) $ betaReduce depth b n
+
+applyReduction :: Express Int -> Express Int
+applyReduction (App (Lam m) n) = applyReduction $ betaReduce 1 m n
+applyReduction (App m n) = applyReduction $ App (applyReduction m) n
+applyReduction a = a
